c665d59a9f8d181f61bf1b85a352f0a8cbe2385b
global-value-0259 <josephtrettevik@gmail.com>
2019-05-26 23:30:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
-// Add your functions and code here
+function destructivelyAppendKitten (name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten (name) {
+  var newElement = [name];
+  var newKittens = kittens.concat(newElement);
+  return newKittens;
+}
+
+function prependKitten (name) {
+  var newElement = [name];
+  var newKittens = newElement.concat(kittens);
+  return newKittens;
+}
+
+function removeLastKitten () {
+  var newKittens = kittens.slice
+}
